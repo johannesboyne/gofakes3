f6ab54e18a747e3b8302cd094533bbb1954b718c
shabbyrobe <code@shabbyrobe.org>
2019-01-02 16:23:30 +1100

    Add WithTimeSource docs
    Actually it's basically a no-op change to force circle to rebuild :D

--- option.go
+++ option.go
@@ -4,6 +4,11 @@ import "time"
 
 type Option func(g *GoFakeS3)
 
+// WithTimeSource allows you to substitute the behaviour of time.Now() and
+// time.Since() within GoFakeS3. This can be used to trigger time skew errors,
+// or to ensure the output of the commands is deterministic.
+//
+// See gofakes3.FixedTimeSource(), gofakes3.LocalTimeSource(tz).
 func WithTimeSource(timeSource TimeSource) Option {
 	return func(g *GoFakeS3) { g.timeSource = timeSource }
 }
